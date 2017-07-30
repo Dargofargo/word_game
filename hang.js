f7f3d71d8642fa4d75625c4d52d1ddc69cd45f40
Dargofargo <dargofargo@gmail.com>
2017-07-30 14:31:48 -0400

    first commit

---
+++ hang.js
@@ -0,0 +1,80 @@
+/**
+ * Created by meriw on 9/9/2016.
+ */
+
+// JavaScript Document
+var $ = function(id) {
+    return document.getElementById(id);
+}
+
+//declare and initialize array
+var game = ["JANUARY", "FEBRUARY", "MARCH", "APRIL"]
+var choice = Math.floor(Math.random()*4);
+var answer = game[choice];
+var myLength = answer.length;
+var display=[myLength];
+var win = myLength;
+var letters = answer.split('');
+var attemptsLeft= 10;
+var output="";
+var userLetter="";
+
+
+
+var setup = function()
+{
+    for (var i=0; i< answer.length; i++)
+    {
+        display[i] = "_ ";
+        output = output + display[i];
+    }
+    document.getElementById("game").innerHTML = output;
+    output ="";
+
+}
+
+var submit = function()
+{
+
+    output = "";
+    userLetter=$("letter").value;
+    $("letter").value ="";
+
+
+    for (var c= 0; c< answer.length; c++)
+    {
+        if (userLetter.toUpperCase() == letters[c])
+        {
+
+            display[c] = userLetter.toUpperCase();
+            win--;
+        }
+        output = output + display[c] + " ";
+
+    }
+    document.getElementById("game").innerHTML = output;
+    output="";
+    attemptsLeft--;
+
+    if (win < 1)
+    {
+        document.getElementById("guesses").innerHTML ="YOU WIN!!!";
+    }
+    else if (attemptsLeft < 1)
+    {
+        document.getElementById("guesses").innerHTML ="YOU LOSE!!!";
+    }
+    else
+    {
+
+        document.getElementById("guesses").innerHTML ="You have " + attemptsLeft + " guesses left";
+    }
+
+}
+
+
+window.onload = function()
+{
+    setup();
+    $("submit").onclick = submit;
+}
